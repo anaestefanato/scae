62b0433c6dfb6abb60587ee43769699d3ac7572b
luisamonequi <luisacazotti.27@gmail.com>
2025-08-05 17:36:25 -0300

    rota 1

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from fastapi.staticfiles import StaticFiles
 # from fastapi.templating import Jinja2Templates
 import uvicorn
 
-
-
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 # templates = Jinja2Templates(directory="templates")
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
-
-# teste
+    
+from routes import public
+app.include_router(public.router)
--- routes/public.py
+++ routes/public.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-from data import produto_repo
 
 
 router = APIRouter()
