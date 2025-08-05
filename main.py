4a646e99020c7ad059db6aab13e0cd5cf83e1f5f
luisamonequi <luisacazotti.27@gmail.com>
2025-08-05 10:38:31 -0300

    chore: Add newline at the end of main.py and remove commented test line

--- main.py
+++ main.py
@@ -17,3 +17,5 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
+
+# teste
