f8c86691c380ad03e7c38b6aa2f3f0ad29e96827
ariannysv <ariannysartev@gmail.com>
2025-08-18 16:27:30 -0300

    feat: initialize additional database tables for user roles
    - Added creation of tables for aluno, administrador, assistente_social, auxilio, chamado, edital, duvida_edital, resposta_chamado, inscricao, notificacao, recurso, auxilio_moradia, and auxilio_transporte in main.py.

--- main.py
+++ main.py
@@ -3,9 +3,23 @@ from fastapi.staticfiles import StaticFiles
 import uvicorn
 from routes import public
 from routes.aluno import perfil_routes
-from repo import usuario_repo
+from repo import administrador_repo, aluno_repo, assistente_social_repo, auxilio_moradia_repo, auxilio_repo, auxilio_transporte_repo, chamado_repo, duvida_edital_repo, edital_repo, inscricao_repo, notificacao_repo, recurso_repo, resposta_chamado_repo, usuario_repo
 
 usuario_repo.criar_tabela()
+aluno_repo.criar_tabela()
+administrador_repo.criar_tabela()
+assistente_social_repo.criar_tabela()
+auxilio_repo.criar_tabela()
+chamado_repo.criar_tabela()
+edital_repo.criar_tabela()
+duvida_edital_repo.criar_tabela()
+resposta_chamado_repo.criar_tabela()
+inscricao_repo.criar_tabela()
+notificacao_repo.criar_tabela()
+recurso_repo.criar_tabela()
+auxilio_moradia_repo.AuxilioMoradiaRepo.criar_tabela()
+auxilio_transporte_repo.AuxilioTransporteRepo.criar_tabela()
+
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
