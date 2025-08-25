e2461341a2bceadaf0606ec9e839f51c1241ef5e
Ana Rosa <estefanatoanarosa@gmail.com>
2025-08-25 17:05:52 -0300

    base2

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Usuario\Desktop\novo\scae\venv
+set VIRTUAL_ENV=C:\Users\20221imi003\Desktop\hoje\scae-1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
