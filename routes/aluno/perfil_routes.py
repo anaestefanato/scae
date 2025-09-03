9c04ee0202c88139c9e9347373e724b98843be88
luisamonequi <luisacazotti.27@gmail.com>
2025-09-03 10:08:45 -0300

    teste de inserção de dados da tabela do banco de dados no html do aluno

--- routes/aluno/perfil_routes.py
+++ routes/aluno/perfil_routes.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
+from repo import aluno_repo, usuario_repo
+
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-
-@router.get("/inicio")
-async def get_root(request: Request):
-    response = templates.TemplateResponse("/aluno/dashboard.html", {"request": request})
+# TESTE COM USUARIO NO LUGAR DE ALUNO PORQUE A TABELA ALUNO NÃO ESTÁ FUNCIONANDO
+@router.get("/inicio/{id}")
+async def get_root(request: Request, id: int):
+    usuario = usuario_repo.obter_por_id(id)
+    response = templates.TemplateResponse("/aluno/dashboard.html", {"request": request, "usuario": usuario})
     return response
 
