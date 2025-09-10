617b3def04ee0d3ff6245541d2788431efd3aa43
ariannysv <ariannysartev@gmail.com>
2025-09-10 16:27:35 -0300

    aluno ok

--- routes/aluno/dados_cadastrais_routes.py
+++ routes/aluno/dados_cadastrais_routes.py
@@ -13,7 +13,7 @@ templates = Jinja2Templates(directory="templates")
 # @requer_autenticacao(["aluno"])
 async def get_dados_cadastrais(request: Request):
     usuario_logado = obter_usuario_logado(request)
-    aluno = usuario_repo.obter_usuario_por_matricula(usuario_logado['matricula'])
+    aluno = usuario_repo.obter_usuario_por_matricula(usuario_logado['matricula'])                 
     response = templates.TemplateResponse("/aluno/dadoscadastrais.html", {"request": request, "aluno": aluno})
     return response
 
