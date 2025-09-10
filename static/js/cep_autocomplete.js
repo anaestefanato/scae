2705d91071d96778855afb97f140d5e4de9fde08
ariannysv <ariannysartev@gmail.com>
2025-09-10 17:08:30 -0300

    Add CEP autocomplete functionality to address fields

---
+++ static/js/cep_autocomplete.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const cepInput = document.querySelector('input[name="cep"]');
+    const cidadeInput = document.querySelector('input[name="cidade"]');
+    const bairroInput = document.querySelector('input[name="bairro"]');
+    const ruaInput = document.querySelector('input[name="rua"]');
+
+    if (cepInput) {
+        cepInput.addEventListener('blur', function() {
+            let cep = cepInput.value.replace(/\D/g, '');
+            if (cep.length === 8) {
+                fetch(`https://viacep.com.br/ws/${cep}/json/`)
+                    .then(response => response.json())
+                    .then(data => {
+                        if (!data.erro) {
+                            if (cidadeInput) cidadeInput.value = data.localidade;
+                            if (bairroInput) bairroInput.value = data.bairro;
+                            if (ruaInput) ruaInput.value = data.logradouro;
+                        }
+                    });
+            }
+        });
+    }
+});
