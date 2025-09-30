663e6916de8e79ebb9d27cdd06617f8fce6c185c
ariannysv <ariannysartev@gmail.com>
2025-09-30 16:15:07 -0300

    troca de nome

--- routes/admin/usuarios_routes.py
+++ routes/admin/usuarios_routes.py
@@ -26,10 +26,26 @@ async def get_usuario_assistente(request: Request, usuario_logado: dict = None):
     response = templates.TemplateResponse("/admin/usuario_assist.html", {"request": request, "admin": admin})
     return response
 
-@router.get("/usuarios/administradores")
+@router.get("/usuarios/admin")
 @requer_autenticacao("admin")
 async def get_usuario_administrador(request: Request, usuario_logado: dict = None):
 
     admin = usuario_repo.obter_usuario_por_matricula(usuario_logado['matricula'])
     response = templates.TemplateResponse("/admin/usuario_admin.html", {"request": request, "admin": admin})
+    return response
+
+@router.get("/usuarios/admin/novo")
+@requer_autenticacao("admin")
+async def get_usuario_administrador_novo(request: Request, usuario_logado: dict = None):
+
+    admin = usuario_repo.obter_usuario_por_matricula(usuario_logado['matricula'])
+    response = templates.TemplateResponse("/admin/novo_admin.html", {"request": request, "admin": admin})
+    return response
+
+@router.get("/usuarios/assistente/novo")
+@requer_autenticacao("admin")
+async def get_usuario_assistente_novo(request: Request, usuario_logado: dict = None):
+
+    admin = usuario_repo.obter_usuario_por_matricula(usuario_logado['matricula'])
+    response = templates.TemplateResponse("/admin/novo_assist.html", {"request": request, "admin": admin})
     return response
