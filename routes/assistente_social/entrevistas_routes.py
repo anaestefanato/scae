b07d01ad97485ceb302046c5608d74c0c64a9757
analuizapassabon <analuizaoliveirapassabon@gmail.com>
2025-10-01 17:05:48 -0300

    entrevistas

--- routes/assistente_social/entrevistas_routes.py
+++ routes/assistente_social/entrevistas_routes.py
@@ -24,3 +24,10 @@ async def get_entrevistas_nova(request: Request, usuario_logado: dict = None):
     assistente = usuario_repo.obter_usuario_por_matricula(usuario_logado['matricula'])
     response = templates.TemplateResponse("/assistente/nova_entrevista.html", {"request": request, "assistente": assistente})
     return response
+
+@router.get("/nova-entrevista")
+@requer_autenticacao("assistente")
+async def get_nova_entrevista(request: Request, usuario_logado: dict = None):
+    assistente = usuario_repo.obter_usuario_por_matricula(usuario_logado['matricula'])
+    response = templates.TemplateResponse("/assistente/nova_entrevista.html", {"request": request, "assistente": assistente})
+    return response
